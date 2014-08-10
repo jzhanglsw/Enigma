a223828a911cd81e67f6150f56071aff0c8cdb13
jzhanglsw <jzhanglsw@gmail.com>
2014-08-10 00:43:41 -0400

    Minor bug fix

--- gen/com/ralphietheman/enigma/R.java
+++ gen/com/ralphietheman/enigma/R.java
@@ -87,14 +87,14 @@ public final class R {
     public static final class string {
         public static final int about=0x7f090005;
         public static final int aboutcontent=0x7f090009;
-        public static final int aboutinstructions=0x7f09000b;
+        public static final int aboutinstructions=0x7f09000a;
         public static final int action_settings=0x7f090001;
-        public static final int answerhint=0x7f09000f;
+        public static final int answerhint=0x7f09000c;
         public static final int app_name=0x7f090000;
-        public static final int cluefiller=0x7f09000e;
+        public static final int cluefiller=0x7f09000b;
         public static final int desc=0x7f090002;
-        public static final int difficulty_title=0x7f090017;
-        public static final int gameplay_title=0x7f090014;
+        public static final int difficulty_title=0x7f090012;
+        public static final int gameplay_title=0x7f09000f;
         public static final int hello_world=0x7f090008;
         public static final int instruct=0x7f090004;
         public static final int options=0x7f090006;
@@ -120,19 +120,19 @@ public final class R {
         public static final int pref_title_sync_frequency=0x7f090022;
         public static final int pref_title_system_sync_settings=0x7f090023;
         public static final int pref_title_vibrate=0x7f090028;
-        public static final int preferences_title=0x7f090016;
-        public static final int settings_title=0x7f090012;
-        public static final int sound_title=0x7f090013;
+        public static final int preferences_title=0x7f090011;
+        public static final int settings_title=0x7f09000d;
+        public static final int sound_title=0x7f09000e;
         public static final int title_activity_about=0x7f090007;
-        public static final int title_activity_instruction=0x7f09000a;
-        public static final int title_activity_play=0x7f09000c;
-        public static final int title_activity_play_menu=0x7f090010;
-        public static final int title_activity_play_menu_activity_old=0x7f090019;
-        public static final int title_activity_puzzle=0x7f09000d;
-        public static final int title_activity_puzzle_activity_old=0x7f090011;
-        public static final int title_activity_puzzle_select=0x7f090018;
+        public static final int title_activity_instruction=0x7f090017;
+        public static final int title_activity_play=0x7f090015;
+        public static final int title_activity_play_menu=0x7f090018;
+        public static final int title_activity_play_menu_activity_old=0x7f090014;
+        public static final int title_activity_puzzle=0x7f090016;
+        public static final int title_activity_puzzle_activity_old=0x7f090019;
+        public static final int title_activity_puzzle_select=0x7f090013;
         public static final int title_activity_settings=0x7f09001a;
-        public static final int welcome_title=0x7f090015;
+        public static final int welcome_title=0x7f090010;
     }
     public static final class style {
         /** 
